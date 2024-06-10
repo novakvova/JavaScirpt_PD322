337083f59c1765042c00e245cb9d9cbde9a5e970
novakvova <novakvova@gmail.com>
2024-06-10 17:00:54 +0300

    Working regsiter is good

--- WebSite/js/forms.js
+++ WebSite/js/forms.js
@@ -35,11 +35,42 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('needs-validation');
     form.addEventListener('submit', event => {
 
+        event.preventDefault(); //підміна стандатної повіденки
         if (!form.checkValidity()) {
-            event.preventDefault(); //підміна стандатної повіденки
             event.stopPropagation();
         } else {
-            event.preventDefault(); ////підміна стандатної повіденки
+            const email = document.getElementById("email").value;
+            const lastName = document.getElementById("lastName").value;
+            const name = document.getElementById("name").value;
+            const exampleInputPassword1 = document.getElementById("exampleInputPassword1").value;
+            const phone = document.getElementById('phone').value;
+            const image = document.getElementById('image');
+
+
+            if (image.files && image.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const imageDataUrl = e.target.result;
+                    let user = {
+                        email,
+                        lastName,
+                        name,
+                        password: exampleInputPassword1,
+                        phone,
+                        image: imageDataUrl
+                    };
+
+                    let users = JSON.parse(localStorage.getItem("users")) || [];
+                    users.push(user);
+                    localStorage.setItem("users", JSON.stringify(users));
+                    window.location.href="/";
+
+                    //alert("Registration successful!");
+                }
+                reader.readAsDataURL(image.files[0]);
+            } else {
+                alert("Please select a profile picture.");
+            }
         }
         form.classList.add('was-validated');
 
